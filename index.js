38665dbe8f30e00d0611be31bfe5310bf6802627
wanderson <wanderson_c_c@hotmail.com>
2020-02-16 08:33:03 -0300

    added fucntion

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ const fileds = document.querySelectorAll("#form-user-create [name]");
 const form = document.querySelector("#form-user-create");
 const user = {};
 
+function addLine(dataUser) {
+
+    let tr = document.createElement('tr');
+    let table = document.querySelector('#table-users')
+
+    tr.innerHTML = ` 
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    `;
+
+    table.appendChild(tr)
+
+   
+
+}
+
 
 form.addEventListener('submit', function(event){
 
@@ -28,7 +53,7 @@ form.addEventListener('submit', function(event){
         
     
     });
-    console.log(user);
+    addLine(user)
     
 });
 
