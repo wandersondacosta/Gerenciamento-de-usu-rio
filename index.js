4bfc178bfa00756b23544c8407d584617b9b87a5
wanderson <wanderson_c_c@hotmail.com>
2020-02-18 22:11:04 -0300

    added UserController

--- index.js
+++ index.js
@@ -1,61 +1,9 @@
-const fileds = document.querySelectorAll("#form-user-create [name]");
-const form = document.querySelector("#form-user-create");
-const user = {};
-
-function addLine(dataUser) {
-
-    let tr = document.createElement('tr');
-    let table = document.querySelector('#table-users')
-
-    tr.innerHTML = ` 
-    <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
-        <td>
-        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-    </tr>
-    `;
-
-    table.appendChild(tr)
-
-   
-
-}
+let userController = new UserController("form-user-create", "table-users");
 
 
-form.addEventListener('submit', function(event){
 
-    event.preventDefault();
-    
 
-    fileds.forEach(function(filed,index){
 
-        if(filed.name == "gender"){
-    
-            if(filed.checked){
-    
-                user[filed.name] = filed.value
-    
-            }; 
-    
-    
-        }else{
-            
-            user[filed.name] = filed.value
-    
-        };
-    
-        
-    
-    });
-    addLine(user)
-    
-});
 
 
 
