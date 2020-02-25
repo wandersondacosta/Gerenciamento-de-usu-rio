ae36b8210cc20bef25c740529fcb1334d45b6488
wanderson <wanderson_c_c@hotmail.com>
2020-02-25 17:04:11 -0300

    added new metody utils

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,10 +7,14 @@ class UserController {
     onSubmit(){
         this.formEl.addEventListener('submit', event => {
             event.preventDefault();
+            let btn = this.formEl.querySelector("[type = submit]");
+            btn.disabled = true;
             let values = this.getValues();
             this.getPhoto().then(content => {
                 values.photo = content;
                 this.addLine(values);
+                this.formEl.reset();
+                btn.disabled = false;
             },
             (e) => {
                 console.error(e);
@@ -72,7 +76,7 @@ class UserController {
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
